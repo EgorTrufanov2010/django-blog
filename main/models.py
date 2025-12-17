d9992507b375ba3901972d4051b3cb47c693771e
Your Name <you@example.com>
2025-12-18 01:54:15 +0400

    Blog 1.0

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)         # заголовок поста
+    created_at = models.DateTimeField(default=timezone.now)  # дата создания
+    content = models.TextField()                      # текст поста
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, 'main/index.html')
+    posts = Post.objects.all().order_by('-created_at')  # самые новые сверху
+    return render(request, 'main/index.html', {'posts': posts})
